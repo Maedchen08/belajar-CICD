a2954dfb217ed25ac1adbc7253cbe21bc96a7467
Maedchen08 <ratihsarifajar18@gmail.com>
2021-10-01 16:25:40 +0700

    add procfile and file yml

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
+import (
+    "log"
+    "os"
+    "github.com/gofiber/fiber/v2"
+)
 
-func IntMin(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    log.Fatal(app.Listen(":"+os.Getenv("PORT")))
+	log.Println("sukses konek")
 }
-
